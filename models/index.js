f41523dbd8d32e58a11aa98ed470a4ac43954cb8
Matt Dambra <mpdambra@gmail.com>
2020-03-12 21:10:04 -0400

    fixing brokenness

--- models/index.js
+++ models/index.js
@@ -9,11 +9,11 @@ const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 if (config.use_env_variable) {
-  let sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  let password = require(__dirname + "/../config/password");
+  var password = require(__dirname + "/../config/password");
   config.password = password;
-  let sequelize = new Sequelize(
+  var sequelize = new Sequelize(
     config.database,
     config.username,
     config.password,
