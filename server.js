becc66cfa281493bdc0a2da10c1686d5f42e3c87
shelby palumbo <shelbypalumbo94@gmail.com>
2020-03-14 23:45:25 -0400

    travis hates const

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 // Requiring necessary npm packages
-const express = require("express");
-const session = require("express-session");
+var express = require("express");
+var session = require("express-session");
 // Requiring passport as we've configured it
-const passport = require("./config/passport"); //testing testing
+var passport = require("./config/passport"); //testing testing
 
 // Setting up port and requiring models for syncing
-const PORT = process.env.PORT || 8080;
-const db = require("./models");
+var PORT = process.env.PORT || 8080;
+var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
-const app = express();
+var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -25,8 +25,12 @@ require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function () {
-  app.listen(PORT, function () {
-    console.log("==> 🌎 Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎 Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
   });
 });
