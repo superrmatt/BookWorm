32e1a4f0a01669784508cf21f44b3a4f317f782f
shelby palumbo <shelbypalumbo94@gmail.com>
2020-03-14 23:53:46 -0400

    trying serving static files relative

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ var db = require("./models");
 var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use("/static", express.static(path.join(__dirname, "public")));
 // We need to use sessions to keep track of our user's login status
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
