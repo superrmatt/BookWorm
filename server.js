4a31a12a122b10f4bf30bdf1771944e56d07445f
shelby palumbo <shelbypalumbo94@gmail.com>
2020-03-15 00:01:55 -0400

    removed unnecessary line

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(express.static("public/stylesheets"));
+
 // We need to use sessions to keep track of our user's login status
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
